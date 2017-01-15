ecbd32128da9e836db622da998b1a83798dea060
Freddi <muff99@outlook.com>
2017-01-16 00:32:15 +0100

    Update upload_gdrive.sh

--- upload_gdrive.sh
+++ upload_gdrive.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo Gdrive start
 
-PREFIX="cm-13.0-"
+PREFIX="lineage-14.1-"
 POSTFIX="-UNOFFICIAL-gemini"
 DATE=`date +%Y%m%d`
 
@@ -9,15 +9,15 @@ PATH="android/system/out/target/product/gemini/"
 GDRIVE_PARENT="0B1UDBYCQNGW7SV9zNGxkYjFHYVk"
 
 
-BUILT=$PREFIX$DATE$POSTFIX".zip"
-MD5=$BUILT".md5sum"
+BUILD=$PREFIX$DATE$POSTFIX".zip"
+MD5=$BUILD".md5sum"
 
 echo "Datum: " $DATE
-echo "Built: " $BUILT
+echo "Build: " $BUILD
 echo "MD5:   " $MD5
 echo "Target:" $PATH
 
-./gdrive upload -p $GDRIVE_PARENT $PATH$BUILT
+./gdrive upload -p $GDRIVE_PARENT $PATH$BUILD
 ./gdrive upload -p $GDRIVE_PARENT $PATH$MD5
 
 echo Gdrive ende
